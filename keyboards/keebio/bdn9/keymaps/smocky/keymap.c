0764b50b31694740fe694a5c30532cc96f918530
smocky <dsmock@gmail.com>
2023-12-13 14:52:10 -0500

    redo BDN9 for OpenAI

--- keyboards/keebio/bdn9/keymaps/smocky/keymap.c
+++ keyboards/keebio/bdn9/keymaps/smocky/keymap.c
@@ -26,8 +26,12 @@ enum encoder_names {
     _RIGHT
 };
 
-#define OSX_SPOTIFY_SHORTCUT LCTL(LALT(LGUI(KC_S)))
-#define OSX_AUDIO_OUTPUT_TOGGLE LGUI(KC_F13)
+#define TOGGLE_SCREEN_APPEARANCE LCTL(LALT(KC_T))
+#define CHATGPT_SHORTCUT LCTL(LALT(KC_C))
+#define GMEET_MUTE_SHORTCUT LGUI(KC_D)
+#define GMEET_VIDEO_SHORTCUT LGUI(KC_E)
+#define SNAGIT_START_CAPTURE LSFT(LCTL(KC_C))
+#define SNAGIT_STOP_CAPTURE LSFT(LCTL(KC_V))
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /*
@@ -37,9 +41,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         | Notification Tray | Toggle Zoom Video, Hold: RGB Control | Toggle Audio Out Device   |
      */
     [_BASE] = LAYOUT(
-        KC_MUTE, TO(_CODE), KC_MPLY,
-        KC_MPRV, KC_MPLY, KC_MNXT,
-        OSX_SPOTIFY_SHORTCUT, LT(_RGB, KC_MPLY), OSX_AUDIO_OUTPUT_TOGGLE),
+        KC_MUTE, TO(_CODE), TOGGLE_SCREEN_APPEARANCE,
+        GMEET_MUTE_SHORTCUT, CHATGPT_SHORTCUT, GMEET_VIDEO_SHORTCUT,
+        SNAGIT_START_CAPTURE, LT(_RGB, KC_MPLY), SNAGIT_STOP_CAPTURE),
     /*
     VSCODE CONTROLS
         | Press: Mute  | Next Layer         | Press: Toggle Zoom Video |
@@ -52,7 +56,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         LGUI(LSFT(KC_M)), LT(_RGB, KC_F11), LGUI(LSFT(KC_F5))
     ),
     [_RGB]    = LAYOUT(
-        RGB_TOG, RGB_MODE_PLAIN, RESET,
+        RGB_TOG, RGB_MODE_PLAIN, QK_BOOT,
         RGB_RMOD, RGB_TOG, RGB_MOD,
         RGB_SAD, _______, RGB_SAI
     )
@@ -78,21 +82,6 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
         }
     } else if (index == _RIGHT) {
         switch (biton32(layer_state)) {
-/*            case _VIDEO:
-                if (clockwise) {
-                    register_code(KC_LCTL);
-                    register_code(KC_LSFT);
-                    tap_code(KC_DOT);
-                    unregister_code(KC_LCTL);
-                    unregister_code(KC_LSFT);
-                } else {
-                    register_code(KC_LCTL);
-                    register_code(KC_LSFT);
-                    tap_code(KC_COMMA);
-                    unregister_code(KC_LCTL);
-                    unregister_code(KC_LSFT);
-                }
-                break;*/
             case _RGB:
                 if (clockwise) {
                     rgblight_increase_hue();
